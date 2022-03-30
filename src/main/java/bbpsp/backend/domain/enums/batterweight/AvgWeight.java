ea88b25a9d324348fb20b7bf62f908fdc9600a3e
Jeong-EuiJae <47394504+cnpcnp99@users.noreply.github.com>
2022-03-31 02:29:50 +0900

    머신러닝 파트에서 연구한 AVG 관련 피쳐들의 웨이트 값을 enum형태로 저장

---
+++ src/main/java/bbpsp/backend/domain/enums/batterweight/AvgWeight.java
@@ -0,0 +1,36 @@
+package bbpsp.backend.domain.enums.batterweight;
+
+public enum AvgWeight {
+    AGE(1.14001536e-06),
+    OBP(4.27877646e-01),
+    SLG(1.40510622e-01),
+    G(9.11344342e-05),
+    PA(2.55649351e-04),
+    AB(-4.19706014e-04),
+    R(-2.37105700e-05),
+    H(4.48246849e-04),
+    _1B(4.92285552e-04),
+    _2B(2.56070581e-04),
+    _3B(-3.28206504e-05),
+    HR(-2.67288633e-04),
+    RBI(-3.29033919e-06),
+    SB(-2.44525468e-05),
+    CS(2.59355509e-05),
+    BB(-7.33110567e-04),
+    HBP(-7.55809287e-04),
+    IBB(-2.08593352e-05),
+    SO(1.15983241e-06),
+    GDP(-4.04268358e-05),
+    SH(-2.10677817e-04),
+    SF(5.76353518e-05);
+
+    private final double value;
+
+    AvgWeight(double value) {
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
